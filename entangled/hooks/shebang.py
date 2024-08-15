191c7c423f0991e90084172855234896f4f8585a
loris-intergalactique <51478929+loris-intergalactique@users.noreply.github.com>
2024-08-15 17:25:17 +0200

    Adapt Entangled.py to Obsidian Code Styler Plugin

--- entangled/hooks/shebang.py
+++ entangled/hooks/shebang.py
@@ -5,6 +5,11 @@ from .base import HookBase
 class Hook(HookBase):
     def on_read(self, code: CodeBlock):
         lines = code.source.splitlines()
+        if len(lines) == 0:
+            return
+
         if lines[0].startswith("#!"):
             code.header = lines[0]
             code.source = "\n".join(lines[1:])
+
+        return
